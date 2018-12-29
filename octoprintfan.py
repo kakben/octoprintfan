cb09e16e94bc6605b2f542ad3aeedc06f60e116c
Martin Hoshi Larsson <martin850102@gmail.com>
2018-12-29 11:44:10 +0000

    Updated temo levels.

--- octoprintfan.py
+++ octoprintfan.py
@@ -16,19 +16,20 @@ try:
     last_switch_low = True
     while True:
         temp = get_cpu_temperature()
-        #print(temp)
-        if temp > 45.0:
+        print(temp)
+        if temp > 39.6:
             GPIO.output(FAN_CTRL_PIN, GPIO.HIGH)
             if last_switch_low == True:
-                #print("Turning on, too hot!")
+                print("Turning fan on, too hot! ({:.1f})".format(temp))
                 last_switch_low = False
-        elif temp < 43.0:
+        elif temp < 38.5:
             GPIO.output(FAN_CTRL_PIN, GPIO.LOW)
             if last_switch_low == False:
-                #print("Turning off, cool enough.")
+                print("Turning fan off, cool enough. ({:.1f})".format(temp))
                 last_switch_low = True
         time.sleep(1)
 except KeyboardInterrupt:
     pass
 finally:
     GPIO.cleanup()
+    print ""
